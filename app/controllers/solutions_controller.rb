7bbce376b707a34a0100f017cd86d47c674029ca
Katherine Cornejo <kathe.cornejo@gmail.com>
2022-02-02 18:08:22 -0500

    solution controller

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,10 +1,20 @@
 class SolutionsController < ApplicationController
+  before_action :set_solution, only: :update
   def create
     @activity = Activity.find(params[:activity_id])
-    @solution = Solution.new
+    @solution = Solution.new(solution_params)
+    @solution.activity = @activity
+    @solution.save
+    redirect_to activity_path(@activity)
   end
 
   def update
 
+  end
+
+  private
+
+  def set_solution
+    @solution = Solution.find(params[:id])
   end
 end
