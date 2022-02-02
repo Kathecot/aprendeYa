5c9f1377968f841d319700840675f09c3da77943
Katherine Cornejo <kathe.cornejo@gmail.com>
2022-02-02 15:32:27 -0500

    backend index courses

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,7 +1,10 @@
 class SolutionsController < ApplicationController
   def create
+    @activity = Activity.find(params[:activity_id])
+    @solution = Solution.new
   end
 
   def update
+
   end
 end
